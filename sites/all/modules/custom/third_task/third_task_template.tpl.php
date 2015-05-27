3b806d1687a5633305c25bcd716ade2c200bfc66
jr-jaguar <j.jaguar@rambler.ru>
2015-05-27 18:10:36 +0300

    Third task. Create page with some text and show it through template

---
+++ sites/all/modules/custom/third_task/third_task_template.tpl.php
@@ -0,0 +1,9 @@
+<div class="third_task_wrapper">
+    <h1>TPL.PHP</h1>
+    <div class ="body">
+        <h3><?php print$variables['f_word'];?>, <?php print$variables['s_word'];?></h3>
+    </div>
+</div>
+
+
+
